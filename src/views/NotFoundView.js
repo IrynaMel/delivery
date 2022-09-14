c0d032e640c24df4dc7cdfa70a9e06dc567149a4
IrynaMel <iryna.melnyk270692@gmail.com>
2022-09-14 16:47:29 +0300

    fix routes

--- src/views/NotFoundView.js
+++ src/views/NotFoundView.js
@@ -2,8 +2,7 @@ import { Link } from 'react-router-dom';
 export default function NotFoundView() {
   return (
     <h1>
-      404 Page Not Found, go to{' '}
-      <Link to="/devoveryApp/home">Delivery page</Link>
+      404 Page Not Found, go to <Link to="/delivery/home">Delivery page</Link>
     </h1>
   );
 }
